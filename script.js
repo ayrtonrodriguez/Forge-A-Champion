a8ab883eb8bc0bbd0a95c674bba3134a44765b9f
Ayrton Rodriguez <97993791+ayrtonrodriguez@users.noreply.github.com>
2026-06-12 16:58:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ const champions = [
 "Annie","Olaf","Galio","Twisted Fate","Xin Zhao","Urgot","LeBlanc","Vladimir",
 "Fiddlesticks","Kayle","Master Yi","Alistar","Ryze","Sion","Sivir","Soraka",
 "Teemo","Tristana","Warwick","Nunu & Willump","Miss Fortune","Ashe","Tryndamere",
-"Jax","Morgana","Zilean","Singed","Evelynn","Twitch","Karthus","Cho'Gath",
+"Jax","Morgana","Zilean","Singed","Evelynn","Twitch","Karthus","Cho Gath",
 "Amumu","Rammus","Anivia","Shaco","Dr. Mundo","Sona","Kassadin","Irelia",
 "Janna","Gangplank","Corki","Karma","Taric","Veigar","Trundle","Swain",
 "Caitlyn","Blitzcrank","Malphite","Katarina","Nocturne","Maokai","Renekton",
 "Jarvan IV","Elise","Orianna","Vayne","Lee Sin","Brand","Rumble","Cassiopeia",
 "Skarner","Heimerdinger","Nasus","Nidalee","Udyr","Poppy","Gragas","Pantheon",
 "Mordekaiser","Ezreal","Shen","Kennen","Garen","Akali","Malzahar","Talon",
-"Riven","Kog'Maw","Shyvana","Lux","Xerath","Ahri","Graves","Fizz","Volibear",
+"Riven","Kog Maw","Shyvana","Lux","Xerath","Ahri","Graves","Fizz","Volibear",
 "Rengar","Varus","Nautilus","Viktor","Sejuani","Fiora","Ziggs","Lulu","Draven",
-"Hecarim","Kha'Zix","Darius","Jayce","Lissandra","Diana","Quinn","Syndra",
-"Aurelion Sol","Kayn","Yasuo","Vel'Koz","Taliyah","Braum","Jhin","Kindred",
+"Hecarim","Kha Zix","Darius","Jayce","Lissandra","Diana","Quinn","Syndra",
+"Aurelion Sol","Kayn","Yasuo","Vel Koz","Taliyah","Braum","Jhin","Kindred",
 "Jinx","Tahm Kench","Senna","Lucian","Zed","Kled","Ekko","Vi","Azir","Yuumi",
-"Thresh","Illaoi","Rek'Sai","Kalista","Bard","Rakan","Xayah","Ornn","Zoe",
-"Kai'Sa","Pyke","Neeko","Sylas","Qiyana","Aphelios","Sett","Lillia","Yone",
-"Samira","Rell","Viego","Gwen","Akshan","Vex","Zeri","Renata Glasc","Bel'Veth",
-"Nilah","K'Sante","Milio","Naafiri","Briar","Hwei","Smolder","Aurora","Ambessa"
+"Thresh","Illaoi","Rek Sai","Kalista","Bard","Rakan","Xayah","Ornn","Zoe",
+"Kai Sa","Pyke","Neeko","Sylas","Qiyana","Aphelios","Sett","Lillia","Yone",
+"Samira","Rell","Viego","Gwen","Akshan","Vex","Zeri","Renata Glasc","Bel Veth",
+"Nilah","K Sante","Milio","Naafiri","Briar","Hwei","Smolder","Aurora","Ambessa"
 ];
 
 const options = {
@@ -31,14 +31,12 @@ let rotation = 0;
 let spinning = false;
 
 function spin() {
-  console.log("spin works");
   if (spinning) return;
 
   spinning = true;
 
   const category = document.getElementById("category").value;
   const list = options[category];
-
   const randomOption = list[Math.floor(Math.random() * list.length)];
 
   const wheel = document.getElementById("wheel");
